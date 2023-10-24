e51fbffaa51734f70f80b57d7e0558e530ad8259
Julian Benegas <julianbenegas99@gmail.com>
2023-10-23 17:12:20 -0700

    alt

--- src/lib/getPokemonFetch.ts
+++ src/lib/getPokemonFetch.ts
@@ -1,29 +1,13 @@
-import { z } from 'zod'
-
-const ZodParser = z.object({
-  results: z
-    .array(
-      z.object({
-        name: z.string(),
-        url: z.string()
-      })
-    )
-    .nonempty({
-      message:
-        "This error is triggered randomly on purpose. When thrown, it should cause the build to fail, but in case the site is revalidating, it shouldn't affect the client experience."
-    })
-})
-
-export type PokeResponse = z.infer<typeof ZodParser>
-
-type GetPokemonFetch = () => Promise<PokeResponse>
-
-export const getPokemonFetch: GetPokemonFetch = async () => {
+export const getPokemonFetch = async () => {
   // If this number is above ~1500 the request will fail on revalidation
   const randomizer = Math.random() * 3200
 
+  if (randomizer > 1500) {
+    throw new Error('intentionally throw an error.')
+  }
+
   const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?offset=${randomizer}&limit=20`,
+    `https://pokeapi.co/api/v2/pokemon?limit=20`,
     {
       next: { revalidate: 1 },
       method: 'GET'
@@ -36,8 +20,5 @@ export const getPokemonFetch: GetPokemonFetch = async () => {
     )
   }
 
-  // If this parsing fails the revalidation should stop too.
-  const data = ZodParser.parse(await response.json())
-
-  return data
+  return await response.json()
 }
